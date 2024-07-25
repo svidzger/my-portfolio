05424db26c5cbbc8df5f0626d7b97829dd64f67a
svidzger <german.svidzinskij@gmail.com>
2024-07-25 09:46:07 +0300

    Add icons for navbar external links and for logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,49 @@
 import { Link } from 'react-router-dom';
-import codingIcon from '../assets/coding-icon.svg';
+import { IconContext } from 'react-icons';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaCode } from 'react-icons/fa6';
 import '../styles/components/navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className='navbar'>
-      <img src={codingIcon} alt='Coding Icon' className='logo' />
-      <ul className='links'>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/projects'>Projects</Link>
-        </li>
-        <li>
-          <Link to='/about'>About</Link>
-        </li>
-        <li>
-          <Link to='/contact'>Contact</Link>
-        </li>
-      </ul>
-      <div className='social-media'>
-        <a
-          href='https://github.com/svidzger'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          GitHub
-        </a>
-        <a
-          href='https://www.linkedin.com/in/german-svidzinskij-12553ab5/'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          LinkedIn
-        </a>
-      </div>
-    </nav>
+    <IconContext.Provider
+      value={{ className: 'social-media-icons', size: '2em' }}
+    >
+      <nav className='navbar'>
+        <FaCode className='logo' />
+        <ul className='links'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/projects'>Projects</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/contact'>Contact</Link>
+          </li>
+        </ul>
+        <div className='social-media'>
+          <a
+            href='https://github.com/svidzger'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaGithub />
+          </a>
+          <a
+            href='https://www.linkedin.com/in/german-svidzinskij-12553ab5/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaLinkedin />
+          </a>
+        </div>
+      </nav>
+    </IconContext.Provider>
   );
 };
 
