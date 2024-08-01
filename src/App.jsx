aef33e2b129fc7bbf25c801956401ba140e1dcdf
svidzger <german.svidzinskij@gmail.com>
2024-08-01 10:39:24 +0300

    Remove unnecessary css import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
 import { navbarRouter } from '../src/routes/routes';
-import './styles/global.css';
 
 const App = () => {
   return <RouterProvider router={navbarRouter} />;
